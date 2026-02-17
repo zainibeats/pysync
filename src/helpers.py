672e6b8ec49e5f130788957971495472848a377e
zainibeats <cheyenne@czaini.net>
2026-02-17 01:23:08 -0800

    check if external

--- src/helpers.py
+++ src/helpers.py
@@ -3,9 +3,12 @@
 import os
 
 
-def is_path_ready(path: str) -> bool:
+def is_path_ready(path: str, external: bool) -> bool:
+    path = os.path.expanduser(path)
     if not os.path.isdir(path):
         return False
+    if not external:
+        return True
     try:
         # Checks if path is mount point
         return bool (os.path.ismount(path))
