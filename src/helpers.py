03f8e876e8f38720deadec3370ddd5d6a75a9d3a
zainibeats <cheyenne@czaini.net>
2026-02-16 22:39:21 -0800

    check for mount points

--- src/helpers.py
+++ src/helpers.py
@@ -7,8 +7,8 @@ def is_path_ready(path: str) -> bool:
     if not os.path.isdir(path):
         return False
     try:
-        # Quick check for non-empty
-        return bool(os.listdir(path))
+        # Checks if path is mount point
+        return bool (os.path.ismount(path))
     except PermissionError:
         # Permission denied → treat as not ready
         return False
