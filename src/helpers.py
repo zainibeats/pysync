70bfafdeaef9c291574215d99342e8b17bd825d4
zainibeats <cheyenne@czaini.net>
2026-03-05 02:56:28 -0800

    error handling

--- src/helpers.py
+++ src/helpers.py
@@ -4,25 +4,32 @@ import sys
 def is_path_ready(path: str, filesystem: str, mount_point=None) -> bool:
     path = os.path.expanduser(path)
     if filesystem == "local":
-        return True
+        return bool (os.listdir(path))
     elif filesystem == "external":
         return bool (os.path.isdir(path))
-    # Filesystem equals NFS
-    else:
+    elif filesystem == "nfs":
         try:
             # Checks if path is mount point
             return bool (os.path.ismount(os.path.expanduser(mount_point)))
         except (PermissionError, TypeError):
             return False
+    else:
+        logger.error(f"Unknown 'filesystem' value in the config.json!")
+        return False
 
 
 def prompt_user(preview_cmd: str) -> bool:
-    while True:
+    attempts = 0
+    while attempts < 4:
         user_input = input(f"You are about to run:\n{preview_cmd}\n\ny or n?: ").strip().lower()
         if user_input == "y":
             return True
         elif user_input == "n":
-            sys.exit()
+            return False
         else:
             print("Enter 'y' to proceed or 'n' to exit")
+            attempts += 1            
             continue
+    else:
+        print("Multiple unknown inputs detected. Quitting PySync...")
+        return False
