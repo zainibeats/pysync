cc35a1085b81280b46c5badb7dc3eda2a41ff7ab
zainibeats <cheyenne@czaini.net>
2026-03-04 22:58:01 -0800

    while true fix

--- src/helpers.py
+++ src/helpers.py
@@ -17,13 +17,12 @@ def is_path_ready(path: str, type: str, mount_point=None) -> bool:
 
 
 def prompt_user(hr_cmd: str) -> bool:
-    user_input = input(f"You are about to run:\n{hr_cmd}\n\ny or n?: ").strip().lower()
-    if user_input == "y":
-        return True
-    elif user_input == "n":
-        sys.exit()
-    else:
-        print("Enter 'y' to proceed or 'n' to exit")
-        continue
-
-
+    while True:
+        user_input = input(f"You are about to run:\n{hr_cmd}\n\ny or n?: ").strip().lower()
+        if user_input == "y":
+            return True
+        elif user_input == "n":
+            sys.exit()
+        else:
+            print("Enter 'y' to proceed or 'n' to exit")
+            continue
