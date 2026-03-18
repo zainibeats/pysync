1294efd9e31e25bc0b707c744053524c1d3e455a
zainibeats <cheyenne@czaini.net>
2026-03-17 23:46:18 -0700

    logging to info

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ os.makedirs(LOG_DIR, exist_ok=True)
 LOG_FILE = os.path.join(LOG_DIR, "pysync.log")
 
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.FileHandler(LOG_FILE),
