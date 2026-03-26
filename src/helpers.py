e19f3620c5bc88e7ff0655ad445d4ffb9c389ea1
zainibeats <cheyenne@czaini.net>
2026-03-26 00:16:46 -0700

    complete config validation

--- src/helpers.py
+++ src/helpers.py
@@ -61,20 +61,78 @@ def validate_config(config: dict) -> bool:
                 return False
             path = source.get('path')
             if path is None:
-                logger.error(f"Missing 'path' key for source {source} in config.json!")
+                logger.error(f"Missing 'path' key for source {name} in config.json!")
                 return False
             filesystem = source.get('filesystem')
             if filesystem is None:
-                logger.error(f"Missing 'filesystem' key for source {source} in config.json!")
+                logger.error(f"Missing 'filesystem' key for source {name} in config.json!")
                 return False
             if 'mount_point' not in source:
-                logger.error(f"Missing 'mount_point' key for source {source} in config.json!")
+                logger.error(f"Missing 'mount_point' key for source {name} in config.json!")
                 return False
+            else:
+                if filesystem == 'nfs':
+                    mount_point = source['mount_point']
+                    if mount_point is None:
+                        logger.error(f"Missing 'mount_point' key for nfs source {name} in config.json! NFS shares must have a defined mount point.")
+                        return False
 
-    # Checks for 'dictionary' key in config.json
+    # Checks for 'destinations' key in config.json
+    destinations = config.get('destinations')
+    if destinations is None:
+        logger.error(f"Missing 'destinations' key in config.json!")
+        return False
+    else:
+        for destination in destinations:
+            name = destination.get('name')
+            if name is None:
+                logger.error(f"Missing 'name' key for destination {destination} in config.json!")       
+                return False
+            path = destination.get('path')
+            if path is None:
+                logger.error(f"Missing 'path' key for destination {name} in config.json!")
+                return False
+            filesystem = destination.get('filesystem')
+            if filesystem is None:
+                logger.error(f"Missing 'filesystem' key for destination {name} in config.json!")
+                return False
+            if 'mount_point' not in destination:
+                logger.error(f"Missing 'mount_point' key for destination {name} in config.json!")
+                return False
+            else:
+                if filesystem == 'nfs':
+                    mount_point = destination['mount_point']
+                    if mount_point is None:
+                        logger.error(f"Missing 'mount_point' key for nfs destination {name} in config.json! NFS shares must have a defined mount point.")
+                        return False
 
     # Checks for 'jobs' key in config.json
+    jobs = config.get('jobs')
+    if jobs is None:
+        logger.error(f"Missing 'jobs' key in config.json!")
+        return False
+    else:
+        for job in jobs:
+            name = job.get('name')
+            if name is None:
+                logger.error(f"Missing 'name' key for job {job} in config.json!")       
+                return False
+            source = job.get('source')
+            if source is None:
+                logger.error(f"Missing 'source' key for job {name} in config.json!")
+                return False
+            destination = job.get('destination')
+            if destination is None:
+                logger.error(f"Missing 'destination' key for job {name} in config.json!")
+                return False
+            if 'exclude_from' not in job:
+                logger.error(f"Missing 'exclude_from' key for job {name} in config.json!")
+                return False
+            if 'flags' not in job:
+                logger.error(f"Missing 'flags' key for job {name} in config.json!")
+                return False
 
+    return True
 
 def resolve_job_paths(job: dict, config: dict) ->  dict:
     
